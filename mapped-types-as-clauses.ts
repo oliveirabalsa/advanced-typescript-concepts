c2e970950daac45bda381b49d7943f4e61a21b62
Leonardo <oliveirabalsa2@gmail.com>
2023-10-04 01:08:01 -0300

    feat(mapped-types-as-clauses): create examples

---
+++ mapped-types-as-clauses.ts
@@ -0,0 +1,34 @@
+export type Setters<State> = {
+  [K in keyof State & string as `set${Capitalize<K>}`]: (
+    value: State[K]
+  ) => void;
+};
+
+export type Getters<State> = {
+  [K in keyof State & string as `get${Capitalize<K>}`]: () => State[K];
+};
+
+export type Store<State> = Setters<State> & Getters<State>;
+
+type PersonState = {
+  name: string;
+  age: number;
+};
+
+type PersonStore = Store<PersonState>;
+
+declare const personStore: PersonStore;
+
+personStore.setName("John");
+personStore.setAge(20);
+const name: string = personStore.getName();
+const age: number = personStore.getAge();
+
+type StoreStyled<StateStyled> = {
+  [K in keyof StateStyled]: () => StateStyled[K];
+};
+
+const PersonStoreStled: StoreStyled<PersonState> = {
+  name: () => "John",
+  age: () => 20,
+};
